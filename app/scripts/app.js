71854d8895a2dbc830069d300191f4bc4f4d2d8a
GiorgioNatili <g.natili@gnstudio.com>
2015-01-04 17:15:43 -0500

    Project scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webrtcStreamMobileFirstApp
+ * @description
+ * # webrtcStreamMobileFirstApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webrtcStreamMobileFirstApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webrtcStreamMobileFirstApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webrtcStreamMobileFirstApp
+ */
+angular.module('webrtcStreamMobileFirstApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
