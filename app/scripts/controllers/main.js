d70ce0f9f953f4897bce29161a4752a5092f5093
GiorgioNatili <g.natili@gnstudio.com>
2015-01-18 16:37:44 -0500

    Attempt to connect using a socket from an iFrame

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,55 @@
  * Controller of the webrtcStreamMobileFirstApp
  */
 angular.module('webrtcStreamMobileFirstApp')
-  .controller('MainCtrl', function ($scope, $document) {
+.controller('MainCtrl', function ($scope, $document, $sce) {
 
-    $document[0].body.addEventListener('touchmove', function(event) {
-       event.preventDefault();
-    }, false); 
+  $document[0].body.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+  }, false); 
 
-    $scope.ipAddress = 'http://192.168.122.1:8080';
+  $scope.$watch('switchStatus', function(newValue){
 
-    $scope.type = '--';
-    $scope.handleGesture = function(event) {
-      console.log(event);
-      $scope.type = event.type;
-    };
+    if(newValue === true){
+
+      var iframe = angular.element( document.querySelector( '#stream-fetcher' ));
+      iframe.sandbox = 'allow-scripts';
+
+      var script = iframe[0].contentWindow.document.createElement('script');
+      script.type = 'text\/javascript';
+
+      script.onload = function(){
+        
+        $scope.currentCameraURL = $sce.trustAsResourceUrl($scope.ipAddress);
+        iframe[0].contentWindow.connector.connect($scope.ipAddress);
+
+      };
+
+      script.onerror = function(error){
+
+        console.log('oh no, the data proxy is not loaded!!!', error);
+
+      };
+
+      cameraIp = $scope.ipAddress;
+
+      script.src = 'scripts/utils/dataProxy.js';
+      iframe.contents().find('body').append(script);
+
+    }
+
+  });
+
+  $scope.ipAddress = 'http://192.168.122.1:60152';
+
+  angular.element(document).ready(function () {
+
+
+  });
+
+  $scope.type = '--';
+  $scope.handleGesture = function(event) {
+    console.log(event);
+    $scope.type = event.type;
+  };
+
   });
